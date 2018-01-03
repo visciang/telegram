6a7320aea68d05647ee3b99bf84a97feee566ecb
Giovanni Visciano <giovanni_visciano@yahoo.it>
2018-01-03 20:55:09 +0100

    fix formatting

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,10 @@
 use Mix.Config
 
 config :tesla,
-    adapter: :mock
+  adapter: :mock
 
 config :telegram, [
-    api_base_url: "http://localhost:8000",
-    get_updates_poll_timeout: 1,
-    on_error_retry_quiet_period: 1
+  api_base_url: "http://localhost:8000",
+  get_updates_poll_timeout: 1,
+  on_error_retry_quiet_period: 1
 ]
