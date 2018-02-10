46343184199c82e6ee68db6271abc1b11bc81a18
Giovanni Visciano <giovanni_visciano@yahoo.it>
2018-02-10 20:06:56 +0100

    test config

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ use Mix.Config
 if Mix.env() == :test do
   config :telegram,
     mock: true,
-    api_base_url: "http://localhost:8000",
+    api_base_url: "http://test:8000",
     get_updates_poll_timeout: 1,
     on_error_retry_quiet_period: 1
 end
