c925b02a7a48c421dff931c46037d43caa2478fb
Giovanni Visciano <giovanni.visciano@statpro.com>
2020-08-04 12:30:59 +0200

    example

--- example/example.exs
+++ example/example.exs
@@ -53,16 +53,19 @@ defmodule Command do
   end
 
   def unknown(token, update) do
-    chat_id = update["message"]["chat"]["id"]
-    message_id = update["message"]["message_id"]
+    unknown_message = "Unknown message:\n\n```\n#{inspect(update, pretty: true)}\n```"
 
-    if chat_id do
-      Telegram.Api.request(token, "sendMessage",
-        chat_id: chat_id,
-        reply_to_message_id: message_id,
-        parse_mode: "MarkdownV2",
-        text: "Unknown message:\n\n```\n#{inspect(update, pretty: true)}\n```"
-      )
+    case update do
+      %{"message" => %{"message_id" => message_id, "chat" => %{"id" => chat_id}}} ->
+        Telegram.Api.request(token, "sendMessage",
+          chat_id: chat_id,
+          reply_to_message_id: message_id,
+          parse_mode: "MarkdownV2",
+          text: unknown_message
+        )
+
+      _ ->
+        Logger.debug(unknown_message)
     end
   end
 end
