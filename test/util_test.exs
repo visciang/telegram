7712da5d69c9bdb362b8236801986789cc35b625
visciang <giovanni_visciano@yahoo.it>
2022-04-02 15:11:04 +0200

    Upgrade deps (#120)

--- test/util_test.exs
+++ test/util_test.exs
@@ -3,14 +3,14 @@ defmodule Test.Telegram.Bot.Utils do
   alias Telegram.Bot.Utils
 
   test "get_from_username" do
-    assert nil == Utils.get_from_username(%{})
+    assert nil == Utils.get_from_username(%{"something" => "else"})
 
     assert {:ok, "test_username"} ==
              Utils.get_from_username(%{"message" => %{"from" => %{"username" => "test_username"}}})
   end
 
   test "get_sent_date" do
-    assert nil == Utils.get_sent_date(%{})
+    assert nil == Utils.get_sent_date(%{"something" => "else"})
 
     datetime = ~U[2015-05-25 13:26:08Z]
     assert {:ok, datetime} == Utils.get_sent_date(%{"message" => %{"date" => DateTime.to_unix(datetime, :second)}})
