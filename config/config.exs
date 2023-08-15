2eec92a96414685b3db1bf25efe8757b949c745f
Giovanni Visciano <giovanni_visciano@yahoo.it>
2023-08-15 15:42:28 +0200

    webserver adapter

--- config/config.exs
+++ config/config.exs
@@ -8,5 +8,6 @@ if config_env() == :test do
   config :telegram,
     api_base_url: "http://test:8000",
     api_max_retries: 2,
-    get_updates_poll_timeout_s: 1
+    get_updates_poll_timeout_s: 1,
+    webserver: Telegram.WebServer.Bandit
 end
---
+++ lib/webserver/bandit.ex
@@ -0,0 +1,30 @@
+defmodule Telegram.WebServer.Bandit do
+  @moduledoc """
+  Bandit child_spec for plug based webserver
+
+  See `Telegram.Webhook`.
+  """
+  alias Telegram.Types
+
+  @spec child_spec(:inet.port_number(), Types.bot_routing()) :: {module(), term()}
+  def child_spec(port, bot_routing_map) do
+    unless Code.ensure_loaded?(Bandit) do
+      # coveralls-ignore-start
+
+      raise """
+      Missing :bandit dependency.
+
+      See Telegram.Webhook documentation.
+      """
+
+      # coveralls-ignore-stop
+    end
+
+    {Bandit,
+     [
+       scheme: :http,
+       plug: {Telegram.Webhook.Router, bot_routing_map},
+       port: port
+     ]}
+  end
+end
