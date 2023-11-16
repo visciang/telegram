23270f87f3d8d582bdb3e5edbb05a5d0e11af0d9
Giovanni Visciano <giovanni_visciano@yahoo.it>
2023-11-16 20:12:06 +0100

    bump 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Telegram.Mixfile do
   def project do
     [
       app: :telegram,
-      version: "1.1.1",
+      version: "1.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [
