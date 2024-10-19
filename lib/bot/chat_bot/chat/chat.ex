682a328629d0ba4049f0276982e47cd0eb8e5379
Giovanni Visciano <giovanni_visciano@yahoo.it>
2024-10-19 08:54:27 +0200

    documentation review

--- lib/bot/chat_bot/chat/chat.ex
+++ lib/bot/chat_bot/chat/chat.ex
@@ -1,7 +1,8 @@
 defmodule Telegram.ChatBot.Chat do
   @moduledoc """
   A struct that represents a chat extracted from a Telegram update.
-  Currently the only required field is id, any other data should be included as keys under the metadata field keyword list.
+  Currently the only required field is `id`, any other data you may want to pass to
+  `c:Telegram.ChatBot.init/1` should be included under the `metadata` field.
   """
 
   @type t() :: %__MODULE__{
